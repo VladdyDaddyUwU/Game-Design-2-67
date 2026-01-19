029879e54469c654d05bf283c41fc58df2793936
Vlad Nico <v.nicolescu@student.tue.nl>
2026-01-19 15:13:42 +0100

    Added some stuff on the new scene

--- Game Design 2/Assets/Scripts/MenuManager.cs
+++ Game Design 2/Assets/Scripts/MenuManager.cs
@@ -3,12 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    // Called by the "PLAY" button
+    public void PlayGame()
+    {
+        // Set to the first level (index 0)
+        LevelData.SelectedLevel = 0; 
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Called by the "QUIT" button
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game Request");
+        Application.Quit();
+    }
+
+    // Existing function (useful if you make a Level Select screen later)
     public void LoadLevel(int levelNumber)
     {
-        // Store the selected level number in our static class
         LevelData.SelectedLevel = levelNumber;
-
-        // Load the main game scene
         SceneManager.LoadScene("SampleScene");
     }
 }
